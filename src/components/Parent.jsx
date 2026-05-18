a24fbf0a901b31e8d7b902113ca526a435144208
pororori <cathy0e0711@gmail.com>
2026-05-18 22:47:30 +0900

    parents 수정

--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
 import Child from "./Child";
 
 function Parent() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <h2>Parent 컴포넌트</h2>
-      <Child count={count} setCount={setCount} />
+      <Child />
     </div>
   );
 }
