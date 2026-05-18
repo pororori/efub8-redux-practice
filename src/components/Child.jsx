818c21403265a249341f5f2b9f0018c9eede5797
pororori <cathy0e0711@gmail.com>
2026-05-18 22:42:09 +0900

    코드수정

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -3,15 +3,16 @@ import { increment, decrement, reset } from "../counterSlice";
 
 function Child() {
   // TODO: Redux 스토어에서 count 값을 가져오세요 (useSelector 사용)
-  const count = null;
+  const count = 0;
   
   // TODO: dispatch 함수를 가져오세요 (useDispatch 사용)
-  const dispatch = null;
+  const dispatch = () => {};
 
   return (
     <div>
       <h2>Child 컴포넌트</h2>
       <p>Count: {count}</p>
+      {/* TODO: 각 버튼에 알맞은 액션을 dispatch 해보세요 */}
       <button onClick={() => dispatch(increment())}>+1</button>
       <button onClick={() => dispatch(decrement())}>-1</button>
       <button onClick={() => dispatch(reset())}>reset</button>
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import Child from "./Child";
 
 function Parent() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h2>Parent 컴포넌트</h2>
-      <Child />
+      <Child count={count} setCount={setCount} />
     </div>
   );
 }
